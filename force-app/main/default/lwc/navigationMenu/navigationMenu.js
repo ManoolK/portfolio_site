dc4cf9066fe13b1a49988544ecdbe6756325e759
Oxana <mail.suvorova.o@gmail.com>
2024-06-09 23:26:17 -0400

    Tune navigation menu

--- force-app/main/default/lwc/navigationMenu/navigationMenu.js
+++ force-app/main/default/lwc/navigationMenu/navigationMenu.js
@@ -1,4 +1,4 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
 import HAMBURGER_ICON from '@salesforce/resourceUrl/hamburgerIcon';
 import X_ICON from '@salesforce/resourceUrl/xIcon';
@@ -13,7 +13,8 @@ export default class NavigationMenu extends NavigationMixin(LightningElement) {
 
     error;
     href = basePath;
-    isLoaded;
+    isLoaded = false;
+    // The menu items when fetched by the NavigationItemsController
     menuItems = [];
     publishedState;
     showHamburgerMenu;
@@ -28,37 +29,36 @@ export default class NavigationMenu extends NavigationMixin(LightningElement) {
     wiredMenuItems({ error, data }) {
         if (data && !this.isLoaded) {
             this.menuItems = data.map((item, index) => {
-                    return {
-                        target: item.Target,
-                        id: index,
-                        label: item.Label,
-                        defaultListViewId: item.DefaultListViewId,
-                        type: item.Type,
-                        accessRestriction: item.AccessRestriction
-                    };
-                }).filter((item) => {
-                    // Only show "Public" items if guest user
-                    return (
-                        item.accessRestriction === 'None' ||
-                        (item.accessRestriction === 'LoginRequired' && !isGuestUser)
-                    );
-                });
+                return {
+                    target: item.Target,
+                    id: index,
+                    label: item.Label,
+                    defaultListViewId: item.DefaultListViewId,
+                    type: item.Type,
+                    accessRestriction: item.AccessRestriction,
+                    windowName: item.target
+                };
+            })
+            .filter((item) => {
+                // Only show "Public" items if guest user
+                return (
+                    item.accessRestriction === 'None' ||
+                    (item.accessRestriction === 'LoginRequired' && !isGuestUser)
+                );
+            });
             this.error = undefined;
             this.isLoaded = true;
         } else if (error) {
             this.error = error;
             this.menuItems = [];
             this.isLoaded = true;
-            console.log(`Navigation menu error: ${JSON.stringify(this.error)}`);
+            console.error(`Navigation menu error: ${JSON.stringify(this.error)}`);
         }
     }
 
     @wire(CurrentPageReference)
     setCurrentPageReference(currentPageReference) {
-        const app =
-            currentPageReference &&
-            currentPageReference.state &&
-            currentPageReference.state.app;
+        const app = currentPageReference && currentPageReference.state && currentPageReference.state.app;
         if (app === 'commeditor') {
             this.publishedState = 'Draft';
         } else {
