4595ff6c5552de1ef37d97017c73eccca382db6f
Oxana <mail.suvorova.o@gmail.com>
2024-06-20 22:27:28 -0400

    Create Hero section. raw css

--- force-app/main/default/lwc/hero/hero.js
+++ force-app/main/default/lwc/hero/hero.js
@@ -2,5 +2,6 @@ import { LightningElement } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/images';
 
 export default class Hero extends LightningElement {
+    funImageURL = IMAGES + '/fun-lines.png';
     personImageURL = IMAGES + '/003.jpg';
 }
