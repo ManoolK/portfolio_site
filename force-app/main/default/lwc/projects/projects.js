92abbed75ef63343faa0aabe65e5016d3b1b257c
Oxana <mail.suvorova.o@gmail.com>
2024-06-28 21:57:46 -0400

    Add 2 other projects. Coming soon

--- force-app/main/default/lwc/projects/projects.js
+++ force-app/main/default/lwc/projects/projects.js
@@ -3,4 +3,5 @@ import IMAGES from '@salesforce/resourceUrl/images';
 
 export default class Projects extends LightningElement {
     siteLWCImageURL = IMAGES + '/lwr_site.png';
+    comingSoonURL = IMAGES + '/coming_soon.png';
 }
