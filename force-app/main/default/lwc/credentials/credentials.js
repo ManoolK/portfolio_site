b428f5218c743c19edd5039f3a5bf53c256faa8c
Oxana <mail.suvorova.o@gmail.com>
2024-07-01 23:16:38 -0400

    Add section My certifications

--- force-app/main/default/lwc/credentials/credentials.js
+++ force-app/main/default/lwc/credentials/credentials.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/images';
 
-export default class Credentials extends LightningElement {}
+export default class Credentials extends LightningElement {
+    pdIImageURL = IMAGES + '/SF-Certified_Platform-Developer-I.png';
+    pAppBuilderImageURL = IMAGES + '/SF-Certified_Platform-App-Builder.png';
+    adminImageURL = IMAGES + '/SF-Certified_Administrator.png';
+}
