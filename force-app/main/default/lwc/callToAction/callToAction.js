374a2f751db9d42af8c79591d1815444ff49f397
Oxana <mail.suvorova.o@gmail.com>
2024-10-10 23:24:49 -0400

    Add to Call to action a property

--- force-app/main/default/lwc/callToAction/callToAction.js
+++ force-app/main/default/lwc/callToAction/callToAction.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class CallToAction extends LightningElement {}
+export default class CallToAction extends LightningElement {
+    @api buttonLabel;
+}
