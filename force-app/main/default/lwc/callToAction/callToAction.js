41cf71bf8abb38ed1bff44bcab1da6725280c2d6
Oxana <mail.suvorova.o@gmail.com>
2024-10-14 23:43:37 -0400

    Start to work on the call to action form

--- force-app/main/default/lwc/callToAction/callToAction.js
+++ force-app/main/default/lwc/callToAction/callToAction.js
@@ -1,5 +1,27 @@
 import { LightningElement, api } from 'lwc';
+import sendMessage from '@salesforce/apex/CallToActionController.sendMessage';
 
 export default class CallToAction extends LightningElement {
     @api buttonLabel;
+    fullname;
+    email;
+    company;
+    message;
+
+    renderedCallback() {
+        this.fullname = this.template.querySelector('.full-name');
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const fullNameValue = this.fullname.value;
+        console.log(fullNameValue);
+
+        sendMessage({
+            fullName: fullNameValue,
+            company: 'com',
+            email: 'test@gmail.com',
+            message: 'test message'
+        });
+    }
 }
