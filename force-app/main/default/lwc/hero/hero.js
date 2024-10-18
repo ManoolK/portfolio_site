b04f3f50485fb6d88a008e708855f19e312f223e
Oxana <mail.suvorova.o@gmail.com>
2024-10-18 16:56:03 -0400

    Added properties for the hero component. Corrected styles

--- force-app/main/default/lwc/hero/hero.js
+++ force-app/main/default/lwc/hero/hero.js
@@ -1,7 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/images';
 
 export default class Hero extends LightningElement {
     funImageURL = IMAGES + '/fun-lines.png';
     personImageURL = IMAGES + '/003.jpg';
+    @api title;
+    @api titleSecond;
+    @api funText;
+    @api smallText;
+    @api showButton1;
+    @api showButton2;
 }
