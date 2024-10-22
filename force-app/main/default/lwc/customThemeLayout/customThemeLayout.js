de8b0f99f51ebb31d714b7e60ef464de31d66610
Oxana <mail.suvorova.o@gmail.com>
2024-10-22 18:52:01 -0400

    Added child-to-parent event to scroll to another component

--- force-app/main/default/lwc/customThemeLayout/customThemeLayout.js
+++ force-app/main/default/lwc/customThemeLayout/customThemeLayout.js
@@ -5,4 +5,18 @@ import { LightningElement } from 'lwc';
  * @slot footer This is the footer slot
  * @slot default This is the default slot
  */
-export default class CustomThemeLayout extends LightningElement {}
+export default class CustomThemeLayout extends LightningElement {
+
+    constructor() {
+        super();
+        this.template.addEventListener('scrolltoevent', this.handleScrollToEvent);
+    }
+
+    handleScrollToEvent = (event) => {
+        if (event) {
+            const componentName = event.detail;
+            const targetSection = this.template.querySelector(`c-${componentName}`);
+            targetSection.scrollIntoView();
+        }
+    }
+}
--- force-app/main/default/lwc/hero/hero.js
+++ force-app/main/default/lwc/hero/hero.js
@@ -4,10 +4,24 @@ import IMAGES from '@salesforce/resourceUrl/images';
 export default class Hero extends LightningElement {
     funImageURL = IMAGES + '/fun-lines.png';
     personImageURL = IMAGES + '/003.jpg';
+    targetSection;
     @api title;
     @api titleSecond;
     @api funText;
     @api smallText;
     @api showButton1;
+    @api button1text;
     @api showButton2;
+    @api button2text;
+    @api componentName;
+
+    handleButton2Click(event) {
+        event.preventDefault();
+        const targetId = event.target.dataset.targetId;
+        const scrollToEvent = new CustomEvent('scrolltoevent', {
+            detail: targetId,
+            bubbles: true
+        });
+        this.dispatchEvent(scrollToEvent);
+    }
 }
