76d1e2a149a20c7da18fdd1a8d44221829a35fa6
Oxana <mail.suvorova.o@gmail.com>
2024-10-29 23:17:07 -0400

    Moved content from projects LWC to projectCard

--- force-app/main/default/lwc/projectCard/projectCard.js
+++ force-app/main/default/lwc/projectCard/projectCard.js
@@ -2,7 +2,49 @@ import { LightningElement, api } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/images';
 
 export default class ProjectCard extends LightningElement {
-    siteLWCImageURL = IMAGES + '/lwr_site.png';
-    @api altText;
-    comingSoonURL = IMAGES + '/coming_soon.png';
+    @api cardtitle;
+    @api carddescription;
+    @api imgfromstaticres;
+    @api alttext;
+    @api imgname;
+    @api applybrand1color;
+
+    get imgLink() {
+        if (this.imgfromstaticres) {
+            return IMAGES + '/' + this.imgname;
+        }
+        return '';
+    }
+
+    get cardClass() {
+        let cardclassmain = 'project project--brand';
+        if (this.applybrand1color) {
+            return cardclassmain + '-1';
+        }
+        return cardclassmain;
+    }
+
+    get linkClass() {
+        let linkClassMain = 'project-title border-bottom-line--brand';
+        if (this.applybrand1color) {
+            return linkClassMain + '-1';
+        }
+        return linkClassMain;
+    }
+
+    get h3Class() {
+        let h3classmain = 'heading-tertiary';
+        if (this.applybrand1color) {
+            return h3classmain + ' border-bottom-line--brand-1';
+        }
+        return h3classmain + ' brand-1';
+    }
+
+    get pClass() {
+        let pclassmain = 'project-descr';
+        if (this.applybrand1color) {
+            return pclassmain + ' project-descr--brand-1';
+        }
+        return pclassmain;
+    }
 }
--- force-app/main/default/lwc/projects/projects.js
+++ force-app/main/default/lwc/projects/projects.js
@@ -1,7 +1,3 @@
 import { LightningElement } from 'lwc';
-import IMAGES from '@salesforce/resourceUrl/images';
 
-export default class Projects extends LightningElement {
-    siteLWCImageURL = IMAGES + '/lwr_site.png';
-    comingSoonURL = IMAGES + '/coming_soon.png';
-}
+export default class Projects extends LightningElement {}
